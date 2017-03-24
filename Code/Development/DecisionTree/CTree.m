fc54b832e957c18df928f6d152c2911c566371a1
jaluce <ja.luce2@gmail.com>
2017-03-24 00:31:26 -0400

    Updated for other data sets and optimizing parameters

--- Code/Development/DecisionTree/CTree.m
+++ Code/Development/DecisionTree/CTree.m
@@ -1,51 +1,94 @@
-opts = detectImportOptions('Nashville_geocoded_processed.csv');
-NashvilleT = readtable('Nashville_geocoded_processed.csv', opts);
+%input files list. Input file s should have the sale price labelled as
+%'Sale_Price'. All spaces and punctuation other than '_' should be removed.
+%All categorical variables should be changed to binary. 
+sets = ['Nashville_geocoded_processed.csv'; 'kc_house_data.csv               '; 'redfin_processed.csv            '];
+datasets =  cellstr(sets);
+datacount = length(datasets);
+MSEs = zeros(1,datacount);
 
-%make new classification, not normalizing because I think it already is
-x = height(NashvilleT);
-TestSplit = 5000;
-NashvilleT.Sale_Class = zeros(x,1);
+for h=1:datacount
+    opts = detectImportOptions(datasets{h});
+    DataTable = readtable(datasets{h}, opts);
 
-%Sort rows first
-NashvilleTSort = sortrows(NashvilleT);
-
-%create N equally sized classes
-classCount = 8;
-classSize = floor(x/classCount);
-splits = zeros(classCount,1);
-
-for t=1:classCount
-    splits(t) = NashvilleTSort.(1)(t*classSize);
-end
-
-for i = 1:x
-    for j = 1:classCount
-        if (NashvilleT.Sale_Price(i) <= splits(j))
-           NashvilleT.Sale_Class(i) = num2str(j);
-           break
+    %fill in missing data with an average. Find column averages first.
+    w = width(DataTable);
+    colaverages = zeros(1,w);
+    for k=2:w
+        coldata = DataTable.(k);
+        colsum = 0;
+        count = 0;
+        for n=1:length(coldata)
+            if (coldata(n)~=-1)
+                colsum = colsum + coldata(n);
+                count = count + 1;
+            end
+            colavg = colsum / count;
+            colaverages(k) = colavg;
         end
     end
+    %now replace with averages.
+    for k=2:w
+        coldata = DataTable.(k);
+        for n=1:length(coldata)
+            if (coldata(n)==-1)
+                DataTable.(k)(n) = colaverages(k);
+            end
+        end
+    end
+
+    %make new classification, not normalizing because I think it already is
+    x = height(DataTable);
+    TestSplit = floor(x*.9);
+    DataTable.Sale_Class = zeros(x,1);
+
+    %Sort rows first
+    DataTSort = sortrows(DataTable);
+
+    %create N equally sized classes
+    classCount = 8;
+    classSize = floor(x/classCount);
+    splits = zeros(classCount,1);
+
+    for t=1:classCount
+        splits(t) = DataTSort.(1)(t*classSize);
+    end
+
+    for i = 1:x
+        for j = 1:classCount
+            if (DataTable.Sale_Price(i) <= splits(j))
+               DataTable.Sale_Class(i) = num2str(j);
+               break
+            end
+        end
+    end
+
+    DataTable.Sale_Price = []; %removing sale price now that we have a sale class
+
+    x = height(DataTable);
+    DataTrain = DataTable(1:TestSplit,:);
+    DataTest = DataTable(TestSplit+1:x,:);
+
+    TestResponse = DataTest.Sale_Class;
+    Response = DataTrain.Sale_Class;
+
+    DataTrain.Sale_Class = [];
+    DataTest.Sale_Class = [];
+
+    %Note, this is picky about column names. Strip out spaces, returns, 
+    %paranethesis, colons, and extra commas
+    CNTree = fitctree(DataTrain, Response, 'Crossval', 'on');
+
+    predictions = predict(CNTree.Trained{1}, DataTest);
+
+    MSEs(h) = loss(CNTree.Trained{1}, DataTest, TestResponse);
+
+    %view(CNTree.Trained{1},'Mode','graph')
+    
+    DataTable = [];
+    
+    TestResponse = [];
+    Response = [];
+
+    DataTrain = [];
+    DataTest = [];
 end
-
-NashvilleT.Sale_Price = []; %removing sale price now that we have a sale class
-
-%removing rows that have missing values for now (rows with a -1 value). 
-%Will make a nearest neighbor replacement function later
-W = width(NashvilleT);
-for y = 1:W
-    toDelete = NashvilleT.(y) == -1;
-    NashvilleT(toDelete,:) = [];
-end
-
-x = height(NashvilleT);
-NashvilleTrain = NashvilleT(1:TestSplit,:);
-NashvilleTest = NashvilleT(TestSplit+1:x,:);
-
-Response = NashvilleTrain.Sale_Class;
-
-%Note, this is picky about column names. Strip out spaces, returns, 
-%paranethesis, colons, and extra commas
-CNTree = fitctree(NashvilleTrain, Response);
-
-CNTree = prune(CNTree);
-[score,cost] = predict(CNTree, NashvilleTest);
--- Code/Development/DecisionTree/RTree.m
+++ Code/Development/DecisionTree/RTree.m
@@ -1,18 +1,56 @@
 opts = detectImportOptions('Nashville_geocoded_processed.csv');
 NashvilleT = readtable('Nashville_geocoded_processed.csv', opts);
 
+%fill in missing data with an average. Find column averages first.
+w = width(NashvilleT);
+colaverages = zeros(1,w);
+for k=2:w
+    coldata = NashvilleT.(k);
+    colsum = 0;
+    count = 0;
+    for n=1:length(coldata)
+        if (coldata(n)~=-1)
+            colsum = colsum + coldata(n);
+            count = count + 1;
+        end
+        colavg = colsum / count;
+        colaverages(k) = colavg;
+    end
+end
+%now replace with averages.
+for k=2:w
+    coldata = NashvilleT.(k);
+    for n=1:length(coldata)
+        if (coldata(n)==-1)
+            NashvilleT.(k)(n) = colaverages(k);
+        end
+    end
+end
+
 %separate data into a training and testing set
 x = height(NashvilleT);
-TestSplit = 10000;
+TestSplit = floor(x*.9);
 NashvilleTrain = NashvilleT(1:TestSplit,:);
 NashvilleTest = NashvilleT(TestSplit+1:x,:);
 
-
 %Sale_Price is the response variable
 responsevar = NashvilleTrain.Sale_Price;
-%Nashville Regression Tree Model
-RNTree = fitrtree(NashvilleTrain,responsevar);
+testresponse = NashvilleTest.Sale_Price;
+NashvilleTrain.Sale_Price = [];
+NashvilleTest.Sale_Price = [];
 
-RNTree = prune(RNTree); 
+%Nashville Regression Tree Model with Cross Validation
+RNTree = fitrtree(NashvilleTrain,responsevar,'Crossval', 'on');
 
-[score,cost] = predict(RNTree, NashvilleTest);
+%Use this line to find optimal parameters. I think this is contributing to
+%overfitting though. Performance is worse when optimized.
+%RNTreeOpt = fitrtree(NashvilleTrain,responsevar,'OptimizeHyperparameters','all');
+
+predictions = predict(RNTree.Trained{1}, NashvilleTest);
+%predictionsOpt = predict(RNTreeOpt, NashvilleTest);
+
+%Calculate Mean Square Error for test set
+MSE = loss(RNTree.Trained{1}, NashvilleTest, testresponse);
+%MSEOpt = loss(RNTreeOpt, NashvilleTest, testresponse);
+
+%view(RNTree.Trained{1},'Mode','graph');
