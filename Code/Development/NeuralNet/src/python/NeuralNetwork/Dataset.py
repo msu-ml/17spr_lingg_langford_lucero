a9a4071ba171d3b966a5fd39c49e9f3753b5eacb
Mick <micklangford@gmail.com>
2017-04-17 23:01:36 -0400

    Changing data structure

--- Code/Development/NeuralNet/src/python/NeuralNetwork/Dataset.py
+++ Code/Development/NeuralNet/src/python/NeuralNetwork/Dataset.py
@@ -12,19 +12,20 @@ class Dataset(object):
     def __init__(self, data, targets):
         self.__num_entries = data.shape[0]
         self.__num_features = data.shape[1]
-        self.__dataset = np.hstack((data, targets))
+        self.__data = data
+        self.__targets = targets
 
     def get_data(self):
-        return self.__dataset[:,:self.num_features]
+        return self.__data
     def set_data(self, v):
-        self.__dataset[:,:self.num_features] = v
+        self.__data = v
     data = property(fget=lambda self: self.get_data(),
                     fset=lambda self, v: self.set_data(v))
     
     def get_targets(self):
-        return self.__dataset[:,self.num_features:]
+        return self.__targets
     def set_targets(self, v):
-        self.__dataset[:,self.num_features:] = v
+        self.__targets = v
     targets = property(fget=lambda self: self.get_targets(),
                        fset=lambda self, v: self.set_targets(v))
         
@@ -35,23 +36,21 @@ class Dataset(object):
     def get_num_features(self):
         return self.__num_features
     num_features = property(fget=lambda self: self.get_num_features())
-
-    def get_num_targets(self):
-        return self.__dataset.shape[1] - self.__num_features
-    num_targets = property(fget=lambda self: self.get_num_targets())
-
+    
     def shuffle(self):
-        np.random.shuffle(self.__dataset)
+        r = np.random.permutation(self.__num_entries)
+        self.__data = self.__data[r]
+        self.__targets = self.__targets[r]
 
     def split(self, ratio):
         n = int(self.num_entries * ratio)
-        dataset1 = Dataset(self.data[:n,:], self.targets[:n,:])
-        dataset2 = Dataset(self.data[n:,:], self.targets[n:,:])
+        dataset1 = Dataset(self.__data[:n,:], self.__targets[:n,:])
+        dataset2 = Dataset(self.__data[n:,:], self.__targets[n:,:])
         return dataset1, dataset2
 
     def create_classes(self, num_classes):
         classes = np.zeros(num_classes)
-        targets = np.sort(np.copy(self.targets), axis=0)
+        targets = np.sort(np.copy(self.__targets), axis=0)
         batch_size = np.int(len(targets) / num_classes)
         num_targets = batch_size * num_classes
         j = 0
@@ -62,11 +61,11 @@ class Dataset(object):
         return classes
 
     def encode_targets(self, classes):
-        targets = self.targets
-        enc_targets = np.zeros((self.num_entries, classes.shape[0]))
+        targets = self.__targets
+        enc_targets = np.zeros((self.__num_entries, classes.shape[0]))
         for i in xrange(self.num_entries):
             enc_targets[i,:] = self.encode_target(targets[i][0], classes)
-        self.__dataset = np.hstack((self.data, enc_targets))
+        self.__targets = enc_targets
     
     def encode_target(self, target, classes):
         target_class = 0
@@ -79,8 +78,8 @@ class Dataset(object):
     
     def make_batches(self, batch_size):
         batches = []
-        for i in xrange(0, self.num_entries, batch_size):
-            batch_data = self.data[i:i+batch_size,:]
-            batch_targets = self.targets[i:i+batch_size,:]
+        for i in xrange(0, self.__num_entries, batch_size):
+            batch_data = self.__data[i:i+batch_size,:]
+            batch_targets = self.__targets[i:i+batch_size,:]
             batches.append(Dataset(batch_data, batch_targets))
         return batches
